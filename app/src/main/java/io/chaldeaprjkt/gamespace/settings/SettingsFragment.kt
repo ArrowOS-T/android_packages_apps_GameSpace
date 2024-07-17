5ef5579cb3634c175d0489298a6564aaea8d84df
minaripenguin <minaripenguin@users.noreply.github.com>
2024-07-17 16:09:36 +0800

    GameSpace: Introduce Quick Start app feature
    * tool for launching apps in freeform mode while gaming etc.
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- app/src/main/java/io/chaldeaprjkt/gamespace/settings/SettingsFragment.kt
+++ app/src/main/java/io/chaldeaprjkt/gamespace/settings/SettingsFragment.kt
@@ -1,5 +1,7 @@
 /*
  * Copyright (C) 2021 Chaldeaprjkt
+ *               2022 crDroid Android Project
+ *               2023-2024 the risingOS Android Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -28,6 +30,8 @@ import io.chaldeaprjkt.gamespace.R
 import io.chaldeaprjkt.gamespace.data.SystemSettings
 import io.chaldeaprjkt.gamespace.preferences.AppListPreferences
 import io.chaldeaprjkt.gamespace.preferences.appselector.AppSelectorActivity
+import io.chaldeaprjkt.gamespace.preferences.QuickStartAppPreference
+import io.chaldeaprjkt.gamespace.preferences.QuickStartAppPreferenceDialogFragment
 import javax.inject.Inject
 
 @AndroidEntryPoint(PreferenceFragmentCompat::class)
@@ -89,5 +93,15 @@ class SettingsFragment : Hilt_SettingsFragment(), Preference.OnPreferenceChangeL
             }
         }
         return false
+    }
+
+    override fun onDisplayPreferenceDialog(preference: Preference) {
+        if (preference is QuickStartAppPreference) {
+            val dialogFragment = QuickStartAppPreferenceDialogFragment.newInstance(preference.key)
+            dialogFragment.setTargetFragment(this, 0)
+            dialogFragment.show(parentFragmentManager, "QuickStartAppPreferenceDialogFragment")
+        } else {
+            super.onDisplayPreferenceDialog(preference)
+        }
     }
 }
